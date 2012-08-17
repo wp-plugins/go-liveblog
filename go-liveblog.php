659da09eea173d303bb89876c048bee013ff5ec1
misterbisson <misterbisson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-17 13:03:57 +0000

    kicking it further down the road
    git-svn-id: https://plugins.svn.wordpress.org/go-liveblog/trunk@586868 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ go-liveblog.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: GigaOM Liveblog
+Plugin URI: http://GigaOM.com/
+Description: GigaOM Liveblog is a solution for covering live events in real time with multiple authors. The page updates live with images and news as it happens..
+Version: a1
+Author: GigaOM
+*/
+
+
